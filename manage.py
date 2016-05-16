fea7dbee742f492d039292ba317c4350f577d34b
tylerlaberge <tyler.laberge@maine.edu>
2016-05-16 13:00:44 -0400

    added cyberattackapp and fixed naming bug in manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cyberblip_old.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cyberblip.settings")
 
     from django.core.management import execute_from_command_line
 
