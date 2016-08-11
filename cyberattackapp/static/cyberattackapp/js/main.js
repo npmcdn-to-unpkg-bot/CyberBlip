50da272fb1921d74c5574116b70996cfe614b5ee
tylerlaberge <tyler.laberge@maine.edu>
2016-08-11 16:11:17 -0400

    made attacks be displayed in simulated real time.

--- cyberattackapp/static/cyberattackapp/js/main.js
+++ cyberattackapp/static/cyberattackapp/js/main.js
@@ -5,12 +5,28 @@ $(document).ready(function () {
         function () {
             var root_frame = new FrameRoot($("body"));
             root_frame.init();
+            var start_time = null;
             $.get('', function (data) {
                 var attacks = JSON.parse(data);
                 for (var i = 0; i < attacks.length; i++){
                     var attack = attacks[i];
-                    var timestamp = attack['timestamp'];
-                    root_frame.handle('attack', attack);
+                    var timestamp = String(attack['timestamp']).split(':');
+                    var this_time = new Date();
+                    this_time.setHours(timestamp[0]);
+                    this_time.setMinutes(timestamp[1]);
+                    this_time.setSeconds(timestamp[2]);
+                    this_time = this_time.getTime();
+                    if (start_time === null){
+                        root_frame.handle('attack', attack);
+                        start_time = this_time
+                    }
+                    else {
+                        (function(curr_attack){
+                            setTimeout(function(){
+                                root_frame.handle('attack', curr_attack);
+                            }, (this_time - start_time));
+                        })(attack);
+                    }
                 }
             });
         }
