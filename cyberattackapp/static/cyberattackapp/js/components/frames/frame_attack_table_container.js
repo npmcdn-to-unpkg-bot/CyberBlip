74e7ec50dfb316fc3ee048df9c4bdec513467ad7
tylerlaberge <tyler.laberge@maine.edu>
2016-08-12 17:04:30 -0400

    added colors to attack table. removed unnecessary nested table and replaced with captions.

--- cyberattackapp/static/cyberattackapp/js/components/frames/frame_attack_table_container.js
+++ cyberattackapp/static/cyberattackapp/js/components/frames/frame_attack_table_container.js
@@ -5,7 +5,6 @@ FrameAttackTableContainer.prototype = Object.create(Frame.prototype);
 FrameAttackTableContainer.prototype.__super__frame__attacke__table__container__ = Frame;
 FrameAttackTableContainer.prototype.init = function () {
     this.add_component('table_attack', new TableAttack($("#table_attack")), true);
-    this.add_component('table_attack_detail', new TableAttackDetail($("#table_attack_detail")), true);
     this.add_component('table_timestamp', new TableTimeStamp($("#table_timestamp")), true);
     this.add_component('table_attacker', new TableAttacker($("#table_attacker")), true);
     this.add_component('table_target', new TableTarget($("#table_target")), true);
