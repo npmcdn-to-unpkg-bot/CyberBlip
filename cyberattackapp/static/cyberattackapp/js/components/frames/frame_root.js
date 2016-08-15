a34875be6ac10df8b56fd6d32b6c2a07f570cf27
tylerlaberge <tyler.laberge@maine.edu>
2016-08-15 13:05:57 -0400

    Made it so that attacks are requested from the backend every minute. Moved more logic into frame root.

--- cyberattackapp/static/cyberattackapp/js/components/frames/frame_root.js
+++ cyberattackapp/static/cyberattackapp/js/components/frames/frame_root.js
@@ -7,9 +7,43 @@ FrameRoot.prototype.init = function () {
     this.add_component('frame_map_container', new FrameMapContainer($('#frame_map_container')), true);
     this.add_component('frame_attack_table_container', new FrameAttackTableContainer($("#frame_attack_table_container")), true);
     this.__super__frame__root.prototype.init.call(this);
+    this.handle('update_attacks');
+    (function(instance){
+        setInterval(function(){
+            instance.handle('update_attacks')
+        }, 60000);
+    })(this);
 };
 FrameRoot.prototype.handle = function (event, data) {
-    if (event === 'attack') {
+    if (event === 'update_attacks') {
+        var start_time = null;
+        (function(instance){
+            $.get('', function (data) {
+                var attacks = JSON.parse(data);
+                for (var i = 0; i < attacks.length; i++){
+                    var attack = attacks[i];
+                    var timestamp = String(attack['timestamp']).split(':');
+                    var this_time = new Date();
+                    this_time.setHours(timestamp[0]);
+                    this_time.setMinutes(timestamp[1]);
+                    this_time.setSeconds(timestamp[2]);
+                    this_time = this_time.getTime();
+                    if (start_time === null){
+                        instance.handle('attack', attack);
+                        start_time = this_time
+                    }
+                    else {
+                        (function(curr_attack){
+                            setTimeout(function(){
+                                instance.handle('attack', curr_attack);
+                            }, (this_time - start_time));
+                        })(attack);
+                    }
+                }
+            });
+        })(this);
+    }
+    else if (event === 'attack') {
         this.components['frame_attack_table_container'].handle('add_attack_row', data);
         this.components['frame_map_container'].handle('attack',
             {
