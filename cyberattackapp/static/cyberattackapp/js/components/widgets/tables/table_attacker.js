b1145b55dcd2157ae4beb5f42e1731eb219506e4
tylerlaberge <tyler.laberge@maine.edu>
2016-08-15 14:00:53 -0400

    added white pusleing highlight effect to newly added attack table rows.

--- cyberattackapp/static/cyberattackapp/js/components/widgets/tables/table_attacker.js
+++ cyberattackapp/static/cyberattackapp/js/components/widgets/tables/table_attacker.js
@@ -5,12 +5,5 @@ TableAttacker.prototype = Object.create(Table.prototype);
 TableAttacker.prototype.__super__table__target__ = Table;
 TableAttacker.prototype.add_body_row = function (data, args) {
     var row = this.__super__table__target__.prototype.add_body_row.call(this, data, args);
-    var start_background_color = row.css('backgroundColor');
-    var start_color = row.css('color');
-    row.css('color', '#FFFFFF');
-    row.css('backgroundColor', '#FFFFFF');
-    row.animate({
-        color: start_color,
-        backgroundColor: start_background_color
-    }, 500);
+    row.pulse({backgroundColor: 'white'}, {pulses: 7, duration: 1000});
 };
--- cyberattackapp/static/cyberattackapp/js/components/widgets/tables/table_target.js
+++ cyberattackapp/static/cyberattackapp/js/components/widgets/tables/table_target.js
@@ -5,12 +5,5 @@ TableTarget.prototype = Object.create(Table.prototype);
 TableTarget.prototype.__super__table__attacker__ = Table;
 TableTarget.prototype.add_body_row = function (data, args) {
     var row = this.__super__table__attacker__.prototype.add_body_row.call(this, data, args);
-    var start_background_color = row.css('backgroundColor');
-    var start_color = row.css('color');
-    row.css('color', '#FFFFFF');
-    row.css('backgroundColor', '#FFFFFF');
-    row.animate({
-        color: start_color,
-        backgroundColor: start_background_color
-    }, 500);
+    row.pulse({backgroundColor: 'white'}, {pulses: 7, duration: 1000});
 };
