0db94d50d9719bf9e70faf001c9e375d98741667
tylerlaberge <tyler.laberge@maine.edu>
2016-08-17 10:58:57 -0400

    lessened decimal places in many cyberattack model fields. added ordering attribute to cyberattack model

--- cyberattackapp/models.py
+++ cyberattackapp/models.py
@@ -7,14 +7,14 @@ class CyberAttack(models.Model):
     """
     timestamp = models.DateTimeField()
     attacker_ip = models.CharField(max_length=100)
-    attacker_latitude = models.DecimalField(max_digits=15, decimal_places=10)
-    attacker_longitude = models.DecimalField(max_digits=15, decimal_places=10)
+    attacker_latitude = models.DecimalField(max_digits=15, decimal_places=6)
+    attacker_longitude = models.DecimalField(max_digits=15, decimal_places=6)
     attacker_location = models.CharField(max_length=100)
-    target_latitude = models.DecimalField(max_digits=15, decimal_places=10)
-    target_longitude = models.DecimalField(max_digits=15, decimal_places=10)
+    target_latitude = models.DecimalField(max_digits=15, decimal_places=6)
+    target_longitude = models.DecimalField(max_digits=15, decimal_places=6)
     target_location = models.CharField(max_length=100)
     service = models.CharField(max_length=100)
-    port = models.DecimalField(max_digits=15, decimal_places=10)
+    port = models.DecimalField(max_digits=15, decimal_places=0)
 
     @classmethod
     def create(cls, **kwargs):
@@ -26,4 +26,8 @@ class CyberAttack(models.Model):
         :rtype: CyberAttack
         """
         return cls(**kwargs)
+
+    class Meta:
+        ordering = ['timestamp']
+
 
