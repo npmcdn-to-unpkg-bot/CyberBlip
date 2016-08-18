4e0a0785108202e513fd3030a6d5db1f01499690
tylerlaberge <tyler.laberge@maine.edu>
2016-08-18 17:16:06 -0400

    updated unit tests

--- cyberattackapp/urls.py
+++ cyberattackapp/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import CyberMapView, CyberAttackView
 
 router = DefaultRouter()
-router.register(r'cyberattacks', CyberAttackView)
+router.register(r'cyberattacks', CyberAttackView, base_name='cyber_attack_view')
 
 urlpatterns = [
     url(r'^$', CyberMapView.as_view(), name='cyber_map_view')
