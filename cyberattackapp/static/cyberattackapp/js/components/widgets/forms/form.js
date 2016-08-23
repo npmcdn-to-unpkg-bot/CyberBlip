5fb9fcff65c322729b4cdbc10a3174a1932fdda9
tylerlaberge <tyler.laberge@maine.edu>
2016-08-23 16:09:50 -0400

    Added pulse and highlight to filters when they are applied.

--- cyberattackapp/static/cyberattackapp/js/components/widgets/forms/form.js
+++ cyberattackapp/static/cyberattackapp/js/components/widgets/forms/form.js
@@ -18,5 +18,19 @@ Form.prototype.submit_form = function () {
 Form.prototype.serialize_form = function () {
     return this.selector.formSerialize();
 };
+Form.prototype.parse_serialized_form = function (serialized_form) {
+    var parsed_data = {};
+    var serialized_data = String(serialized_form).split('&');
+    for (var i = 0; i < serialized_data.length; i++){
+        var data = String(serialized_data[i]).split('=');
+        if (data[1] != ''){
+            parsed_data[data[0]] = data[1];
+        }
+    }
+    return parsed_data;
+};
+Form.prototype.select_input_by_name = function (name) {
+    return this.selector.find(':input[name="' + String(name) + '"]');
+};
 
 
--- cyberattackapp/static/cyberattackapp/js/components/widgets/forms/form_attack_filter.js
+++ cyberattackapp/static/cyberattackapp/js/components/widgets/forms/form_attack_filter.js
@@ -5,11 +5,29 @@ FormAttackFilter.prototype = Object.create(Form.prototype);
 FormAttackFilter.prototype.__super__form__attack__filter__ = Form;
 FormAttackFilter.prototype.init = function () {
     this.__super__form__attack__filter__.prototype.init.call(this);
+    this.filtered_original_colors = {};
     (function(instance){
         instance.selector.find(':input').keypress(function (e) {
             if (e.which == 13) {
                 var serialized_form = instance.serialize_form();
                 instance.handle('apply_filter', serialized_form);
+                var input_fields = instance.selector.find(':input');
+                for (var key in instance.filtered_original_colors) {
+                    if (instance.filtered_original_colors.hasOwnProperty(key)){
+                        var input_field = instance.select_input_by_name(key);
+                        input_field.css('backgroundColor', instance.filtered_original_colors[key]);
+                        delete instance.filtered_original_colors[key];
+                    }
+                }
+                input_fields.pulse({backgroundColor: 'white'}, {pulses: 1, duration: 500});
+                var form_data = instance.parse_serialized_form(serialized_form);
+                for (var key in form_data){
+                    if (form_data.hasOwnProperty(key)){
+                        var input_field = instance.select_input_by_name(key);
+                        instance.filtered_original_colors[key] = input_field.css('backgroundColor');
+                        input_field.stop().css('backgroundColor', 'white');
+                    }
+                }
             }
         });
     })(this);
